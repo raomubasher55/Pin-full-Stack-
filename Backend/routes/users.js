2d8f97fe236b25b549b2fbeaf03765b4b98f5690
raomubasher55 <raomubasher5555@gmail.com>
2024-05-06 10:16:12 -0700

    try for .env

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose');
-require('dotenv').config();
 // mongoose.connect('mongodb://localhost:27017/pin');
-// const DB = 'mongodb+srv://raomubasher5555:Rao3937!@cluster0.07tu9yq.mongodb.net/pin?retryWrites=true&w=majority&appName=Cluster0'
-
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
+const DB = 'mongodb+srv://raomubasher5555:Rao3937!@cluster0.07tu9yq.mongodb.net/pin?retryWrites=true&w=majority&appName=Cluster0'
+ 
+mongoose.connect(DB)
+.then(() => {
+  console.log("Connected to MongoDB");
+})
+.catch((err) => {
+  console.error("Error connecting to MongoDB:", err);
+});
 
 
 const userSchema = new mongoose.Schema({
